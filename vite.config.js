f354db3996869590598499a262facd598b381edd
jhongonzalezs <jhongonzalezs2001@hotmail.com>
2024-05-11 20:39:04 -0500

    add deploy.yml

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "web-react-route",
+  base: "/Web_React_Route/",
   plugins: [react()],
 })
