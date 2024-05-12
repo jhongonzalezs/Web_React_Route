75e598665415e11a3224d3e7d620a9ace3292873
jhongonzalezs <jhongonzalezs2001@hotmail.com>
2024-05-11 20:08:35 -0500

    Cambio

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "web_react_route",
+  base: "web-react-route",
   plugins: [react()],
 })
